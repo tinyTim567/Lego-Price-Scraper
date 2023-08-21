b34828a4a6fcb0a325b95f8ed09eed864b501fa7
tinyTim567 <tmck911@gmail.com>
2023-08-21 15:48:05 +0100

    Code cleanup

--- main.py
+++ main.py
@@ -38,7 +38,6 @@ if __name__ == '__main__':
 ==================================================
         """)
 
-    print("\n\n\n\n\n\n")
     export_all_to_csv(url, creation_date)
 
     input(f"""\n\n\n\n\n\n
